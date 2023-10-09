5aaa0b2dff0a850bf3f993c26e4896aa54210a23
PNiemi <pauliina.niemi@kapsi.fi>
2023-10-09 18:16:05 +0300

    step 2 & 3

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
+const Header = ({headerTxt}) => (
+  <h2>{headerTxt}</h2>
+)
+
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
-const App = () => {
+const ShowVotes = ({votes}) => (
+  <p>Has {votes} votes</p>
+)
+
+const App = () => { 
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -17,17 +25,34 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+  let initPoints = Array(anecdotes.length).fill(0) 
   const [selected, setSelected] = useState(0)
   const selectAnecdote = () => setSelected(Math.floor(Math.random()*anecdotes.length))
+  const [points, setPoints] = useState(initPoints)
+  const vote = () => {
+    const newPoints = [...points];
+    newPoints[selected] += 1
+    setPoints(newPoints)
+  }
+
+  const mostVotes = () => points.indexOf(Math.max(...points))
 
   return (
     <div>
+      <Header headerTxt="Anecdote of the day"/>
       {anecdotes[selected]}
       <br />
+      <ShowVotes votes={points[selected]} />
+      <Button
+        handleClick={vote}
+        text="vote" />
       <Button
         handleClick={selectAnecdote}
         text="next anecdote" />
+      <Header headerTxt="Anecdote with most votes"/>
+      {anecdotes[mostVotes()]}
+      <br />
+      <ShowVotes votes={points[mostVotes()]} />
     </div>
   )
 }
