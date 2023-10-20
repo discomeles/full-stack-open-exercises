3c905615f921892f3cb4d7a246d7b3d3d5840a82
PNiemi <pauliina.niemi@kapsi.fi>
2023-10-20 08:42:56 +0300

    puhelinluettelo step 4

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -4,12 +4,17 @@ const App = () => {
   // App-komponentille määritellään tila, joka saa alkuarvoksi
   // luettelon alustavan taulukon
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231234' }
+    { name: 'Arto Hellas', number: '040-1231234' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   
   // Tila newName kontrolloi lomaketta 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  // filterValue kontrolloi filter kenttää
+  const [filterValue, setFilterValue] = useState('')
 
   // Lomakkeen tapahtumankäsittelijä
   // event.preventDefault() estää oletusarvoisen toiminnan
@@ -38,19 +43,39 @@ const App = () => {
     setNewNumber('')
   }
 
-  // Syötekomponentin tapahtumankäsittelijä synkronoi
+  // Nimisyötekomponentin tapahtumankäsittelijä synkronoi
   // nimikenttään tehdyt muutokset App-komponentin tilaan
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  // Number syötekomponentin tapahtumankäsittelijä
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
 
+  // Filter syötekomponentin tapahtumankäsittelijä
+  const handleFilter = (event) => {
+    setFilterValue(event.target.value)
+  }
+
+  // Rajataan näytettävät niment filtterin arvon mukaan
+  const filteredPersons = persons.filter((element) => (
+  element.name.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase())))
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      {/* Hakukenttä */}
+      filter shown with <input 
+                          type="text"
+                          name="filter"
+                          id="filter"
+                          autoComplete='off'
+                          value={filterValue}
+                          onChange={handleFilter}
+                          />
+      <h2>add a new</h2>
       {/* Nimilomakkeen tapahtumankäsittelijä on addName */}
       <form onSubmit={addName}>
         <div>
@@ -81,7 +106,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {/* Näytetään suodatetut tiedot*/}
+      {filteredPersons.map(person =>
         <p key={person.name}>{person.name} {person.number}</p>
         )}
     </div>
