6b6a29b90a94b2762d052c3898239509936b0bc0
PNiemi <pauliina.niemi@kapsi.fi>
2023-11-29 10:54:22 +0200

    Osa 2, tehtävä 2.20 maiden tiedot, step3 (sää)

--- osa2/maatiedot/src/components/CountryInfo.jsx
+++ osa2/maatiedot/src/components/CountryInfo.jsx
@@ -1,8 +1,25 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
+const owapi_key = import.meta.env.VITE_OWAPI_KEY
+
 const CountryInfo = ({selected}) => {
   const [countryData, setCountryData] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
+
+  // Säädatan haku
+  const getWeather = (latlng) => {
+    // console.log(latlng)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&appid=${owapi_key}&units=metric`
+    axios
+      .get(url)
+        .then(response => {
+          setWeatherData(response.data)
+          // console.log(response.data.main.temp)
+          // console.log(response.data.wind.speed)
+          // console.log(response.data.weather[0].icon)
+        })
+  }
 
   // Haetaan maakohtainen data
   useEffect(() => {
@@ -11,10 +28,11 @@ const CountryInfo = ({selected}) => {
       .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${selected.toLocaleLowerCase()}`)
         .then(response => {
           setCountryData(response.data)
+          getWeather(response.data.capitalInfo.latlng)
         })}
     },[selected])  
 
-  if (countryData) {
+  if (countryData && weatherData) {
     return (
       <>
       <h1>{countryData.name.common}</h1>
@@ -24,13 +42,21 @@ const CountryInfo = ({selected}) => {
       area {countryData.area}
       </p>
       <h2>languages:</h2>
+      <ul>
       {Object.values(countryData.languages).map(lang => 
         <li key={lang}>
           {lang}
         </li>
         )}
+      </ul>
         <br/>
       <img src={countryData.flags.png} alt={countryData.flags.alt} height="150"/>
+      <h2>Weather in {countryData.capital[0]}</h2>
+      <p>
+        temperature {weatherData.main.temp} Celcius<br/>
+        <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} /><br/>
+        wind {weatherData.wind.speed} m/s 
+      </p>
       </>
   )
   } else {
