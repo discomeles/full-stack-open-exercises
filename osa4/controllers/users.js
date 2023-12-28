49ed65f688d7a0b0d6b157ca44accd226c3eb785
PNiemi <pauliina.niemi@kapsi.fi>
2023-12-28 16:58:45 +0200

    Osa4, tehtävä 4.15 blogilistan laajennus, step3, käyttäjän luominen

---
+++ osa4/controllers/users.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const usersRouter = express.Router()
+const User = require('../models/user')
+
+// --- Lisää käyttäjä ---
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+// --- Hae kaikki käyttäjät ---
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+module.exports = usersRouter
