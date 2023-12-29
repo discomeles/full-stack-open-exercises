c679ecbf78c25c9d52276791381ac1907afb724a
PNiemi <pauliina.niemi@kapsi.fi>
2023-12-29 18:01:03 +0200

    Osa4, tehtävä 4.19 blogilistan laajennus, step7, token ja blogin lisäys

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,6 +2,16 @@ const express = require('express')
 const blogRouter = express.Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+const getToken = request => {
+  const authorization = request.headers.authorization
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 // --- Blogi API ---
 // --- Hae kaikki ---
@@ -15,25 +25,31 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response, next) => {
   if (!request.body.title) {
     return response.status(400).end()
-  } else if (!request.body.url) {
+  } 
+  if (!request.body.url) {
     return response.status(400).end()
-  } else {
-    const users = await User.find({})
-    const user = users[0]
-
-    const blog = new Blog({
-      title: request.body.title,
-      author: request.body.author,
-      url: request.body.url,
-      likes: request.body.likes ? request.body.likes : 0,
-      user: user._id
-    })
-      const savedBlog = await blog.save()
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
-
-      response.status(201).json(savedBlog)
   }
+  const decodedToken = jwt.verify(getToken(request), config.jwtsecret)
+  console.log(decodedToken)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes ? request.body.likes : 0,
+    user: user._id
+  })
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  
 })
 
 // --- Poista blogi id:n perusteella ---
