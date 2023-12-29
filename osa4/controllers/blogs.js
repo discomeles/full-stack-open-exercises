7c2a76f7df8ae6380f9e41471d214d9be85bc74e
PNiemi <pauliina.niemi@kapsi.fi>
2023-12-29 16:16:12 +0200

    Osa4, tehtävä 4.17 blogilistan laajennus, step5, blogit käyttäjille ja käyttäjät blogeille

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const blogRouter = express.Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // --- Blogi API ---
 // --- Hae kaikki ---
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', {username: 1, name: 1, id: 1})
   response.json(blogs)
 })
 
@@ -16,14 +18,21 @@ blogRouter.post('/', async (request, response, next) => {
   } else if (!request.body.url) {
     return response.status(400).end()
   } else {
-  const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes ? request.body.likes : 0
-  })
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+    const users = await User.find({})
+    const user = users[0]
+
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes ? request.body.likes : 0,
+      user: user._id
+    })
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+
+      response.status(201).json(savedBlog)
   }
 })
 
--- osa4/controllers/users.js
+++ osa4/controllers/users.js
@@ -31,7 +31,8 @@ usersRouter.post('/', async (request, response) => {
 
 // --- Hae kaikki käyttäjät ---
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1, id:1 })
   response.json(users)
 })
 
