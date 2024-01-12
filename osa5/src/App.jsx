9796a3ddbbee1ebe4d8e999f9160abe354ec6b64
PNiemi <pauliina.niemi@kapsi.fi>
2024-01-12 10:24:28 +0200

    Osa5, tehtävä 5.2 blogilistan frontend, step2, kirjautunut käyttäjä local storageen

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -21,6 +21,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -30,7 +33,12 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
 
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -66,7 +74,8 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>
-      {user.name} logged in
+      {user.name} logged in 
+      <button type="button" onClick={handleLogout}>logout</button>
       </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
