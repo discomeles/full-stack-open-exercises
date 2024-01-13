a652e534f04f58677927992ff8e1456f980ee622
PNiemi <pauliina.niemi@kapsi.fi>
2024-01-13 17:55:25 +0200

    osa5, tehtävä 5.7 blogilistan fronted, step7, kaikkien blogin tietojen näyttäminen

--- osa5/src/components/Blog.jsx
+++ osa5/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showAll, setShowAll] = useState(false)
+
+  const toggleShowAll = () => {
+    const value = showAll === true ? false : true
+    setShowAll(value)
+  }
+
+  const buttonText = showAll === false ? "view" : "hide"
+
+  const blogInfo = () => (
+    <>
+    <br/>
+    {blog.url}<br/>
+    {blog.likes} <button type="button">like</button><br/>
+    {blog.user.name}<br/>
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button 
+                                    type="button" 
+                                    onClick={(e) => toggleShowAll()}>{buttonText}</button>
+      {showAll && blogInfo()}
+    </div>  
+  )
+
+}
+
 
 export default Blog
