d124550b9a9688f8d921ddc1378cf018d9a265c5
PNiemi <pauliina.niemi@kapsi.fi>
2024-01-15 18:09:27 +0200

    Osa5, tehtävä 5.10 blogilistan frontend, step10, blogit like-järjestykseen

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -153,7 +153,7 @@ const App = () => {
                           addBlog={addBlog} 
                           showBlogForm={showBlogForm} 
                           toggleBlogForm={toggleBlogForm}/>}
-      {blogs.map(blog =>
+      {blogs.sort((a,b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
       )}
       
