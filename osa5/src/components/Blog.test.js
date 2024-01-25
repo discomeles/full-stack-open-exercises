8283de87bfb58685ac34783c9235e2b2a1c0b96f
PNiemi <pauliina.niemi@kapsi.fi>
2024-01-25 09:24:22 +0200

    Osa5, tehtävä 5.14 blogilistan testit, step2, näytä kaikki tiedot + 5.13 korjaus

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title', () => {
@@ -25,6 +26,46 @@ test('renders title', () => {
 
   render(<Blog blog={blog} user={user}/>)
 
-  const element = screen.findByText('Go To Statement Considered Harmful')
+  const element=screen.getByText('Go To Statement Considered Harmful Edsger W. Dijkstra')
+  expect(element).toBeDefined()
 
+})
+
+test('clicking the view button shows all blog info', async () => {
+  const blog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    user: {
+      username: 'test',
+      name: 'test',
+      id: '1'
+    },
+    id: '65a006c5d43eb7c738934d47'
+  }
+
+  const user = {
+    username: 'test',
+    name: 'test',
+    id: '1'
+  }
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blog={blog} user={user}/>
+  )
+
+  const userev = userEvent.setup()
+  const button = screen.getByText('view')
+  await userev.click(button)
+
+  const urlElement = screen.getByText('http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html')
+  const likeElement = screen.getByText('likes 5')
+  const userElement = screen.getByText('test')
+
+  expect(urlElement).toBeDefined()
+  expect(likeElement).toBeDefined()
+  expect(userElement).toBeDefined()
 })
