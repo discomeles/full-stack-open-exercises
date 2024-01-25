aa20e8b423fcf6354ced0d7056f8b3dc55fcf0ed
PNiemi <pauliina.niemi@kapsi.fi>
2024-01-25 10:11:42 +0200

    Osa5, tehtävä 5.15 blogilistan testit, step3, kaksi napin painallusta kutsuu handleria kaksi kertaa

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -51,8 +51,6 @@ test('clicking the view button shows all blog info', async () => {
     id: '1'
   }
 
-  const mockHandler = jest.fn()
-
   render(
     <Blog blog={blog} user={user}/>
   )
@@ -68,4 +66,41 @@ test('clicking the view button shows all blog info', async () => {
   expect(urlElement).toBeDefined()
   expect(likeElement).toBeDefined()
   expect(userElement).toBeDefined()
+})
+
+test('clicking like button twice calls event handler twice', async () => {
+  const blog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    user: {
+      username: 'test',
+      name: 'test',
+      id: '1'
+    },
+    id: '65a006c5d43eb7c738934d47'
+  }
+
+  const user = {
+    username: 'test',
+    name: 'test',
+    id: '1'
+  }
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blog={blog} updateBlog={mockHandler} user={user} />
+  )
+
+  const userev = userEvent.setup()
+  const button = screen.getByText('view')
+  await userev.click(button)
+
+  const likeButton = screen.getByText('like')
+  await userev.click(likeButton)
+  await userev.click(likeButton)
+  expect(mockHandler.mock.calls).toHaveLength(2)
+
 })
