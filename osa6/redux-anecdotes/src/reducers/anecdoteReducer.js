e28727b538acdf14e9f1268f687f94019a9a0652
PNiemi <pauliina.niemi@kapsi.fi>
2024-01-26 13:43:38 +0200

    Osa6, tehtävä 6.4 anekdootit, step2, anekdootin lisääminen

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -32,6 +32,8 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
     }
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
     default:
       return state
   }
@@ -41,6 +43,14 @@ export const voteAnecdote = (id) => {
   return {
     type: 'VOTE',
     payload: {id}
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: asObject(content)
+    
   }
 }
 
